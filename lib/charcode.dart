d1d02b1f4117fd8b9b48b767ffafc8ab1775ff27
Lasse R.H. Nielsen <lrn@chromium.org>
2014-02-28 15:33:31 +0100

    Version one.

---
+++ lib/charcode.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/**
+ * Defines symbolic names for character code points.
+ *
+ * Includes all ASCII and Latin-1 characters.
+ *
+ * Exports the libraries `ascii.dart` and `html-entity.dart`.
+ */
+library charcode;
+
+export "ascii.dart";
+export "html-entity.dart";
