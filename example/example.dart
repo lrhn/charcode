1fd1f7690b77ba97497e463cccff84d4b2755938
Kevin Moore <kevmoo@google.com>
2020-02-10 20:23:47 -0800

    Add an example

---
+++ example/example.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2020, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:charcode/charcode.dart';
+
+void main() {
+  print(String.fromCharCodes([
+    $E,
+    $x,
+    $a,
+    $m,
+    $p,
+    $l,
+    $e,
+    $exclamation,
+  ]));
+}
