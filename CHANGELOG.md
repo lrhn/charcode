bcd8a12c315b7a83390e4865ad847ecd9344cba2
Lasse R.H. Nielsen <lrn@google.com>
2020-09-22 17:06:23 +0200

    Command line functionality (#17)
    * Add command-line functionality to generate constants.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## 1.2.0-nullsafety.2
+
+- Add command line functionality to generate constants.
+  Allows clients to generate their own constants instead of
+  depending on the package at run-time.
+  Example: To generate the characters needed
+  for a hexadecimal numeral, run `pub run charcode a-fA-F\d+-`.
+
 ## 1.2.0-nullsafety.1
 
 - Allow 2.10 stable and 2.11.0 dev SDK versions.
@@ -8,7 +16,7 @@
 
 ## 1.1.3
 
-- Added example, fixed recommended lints.
+- Added example, changed lints.
 
 ## 1.1.2
 
@@ -16,9 +24,7 @@
 
 ## 1.1.1
 
-- Spelling fixes.
-
-- Linting fixes.
+- Spelling and linting fixes.
 
 ## 1.1.0
 
--- example/example.dart
+++ example/example.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:charcode/charcode.dart';
+import "package:charcode/charcode.dart";
 
 void main() {
   print(String.fromCharCodes([
@@ -15,4 +15,19 @@ void main() {
     $e,
     $exclamation,
   ]));
+}
+
+/// Check whether `(` and `)` are balanced in [input].
+bool checkBalancedParentheses(String input) {
+  var openParenCount = 0;
+  for (var i = 0; i < input.length; i++) {
+    var char = input.codeUnitAt(i);
+    if (char == $lparen) {
+      openParenCount++;
+    } else if (char == $rparen) {
+      openParenCount--;
+      if (openParenCount < 0) return false;
+    }
+  }
+  return openParenCount == 0;
 }
